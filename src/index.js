88a3541d5a77d28c2bd781bd632b9f070a162af8
Andrey <priv1984@yandex.ru>
2022-01-25 00:24:14 +0700

    Training project. Learning React JS. Social network. GIT. 92 lesson

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
+
             <Provider store={store}>
                 {/*<App store={store} state={state} dispatch={store.dispatch.bind(store)}/>*/}
                 <App/>
 
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
